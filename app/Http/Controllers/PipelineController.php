5074e681110eabd7ef69f562cd7a113e13538618
HayanoRyuki <hayano@media-confidence.com>
2025-12-26 12:19:52 +0900

    perf: eager load applications and relations in pipeline controller

--- app/Http/Controllers/PipelineController.php
+++ app/Http/Controllers/PipelineController.php
@@ -9,12 +9,17 @@ class PipelineController extends Controller
     public function show(Job $job)
     {
         $steps = $job->selectionSteps()
+            ->with([
+                'applications.candidate',
+                'applications.hiringDecision',
+                'applications.selectionStep',
+            ])
             ->orderBy('order')
             ->get();
 
         $applicationsByStep = $steps->mapWithKeys(function ($step) {
             return [
-                $step->id => $step->applications()->get(),
+                $step->id => $step->applications,
             ];
         });
 
