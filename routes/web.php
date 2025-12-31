c4dc9c0c8aa12ec88dda0e1d0905f056f0f10b64
HayanoRyuki <hayano@media-confidence.com>
2025-12-31 09:59:04 +0900

    fix(ats): redirect / and /login to Auth SSO start

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,23 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Root (always redirect to Auth SSO start)
+| Root / Login (always redirect to Auth SSO start)
 |--------------------------------------------------------------------------
 |
-| 未ログインで ATS に直アクセスされた場合は、
-| 必ず Auth アプリの SSO 開始画面へリダイレクトする
-|
-| ※ SSO callback は「必ず絶対URLで固定」する
+| ATS はログイン画面を持たない。
+| 直打ち・/login いずれも必ず Auth の SSO に飛ばす。
 |
 */
-Route::get('/', function () {
+$redirectToAuth = function () {
     return redirect()->away(
         'https://auth.opinio.co.jp/sso/start'
         . '?client_id=ats'
         . '&redirect_uri=' . urlencode('https://ats.opinio.co.jp/sso/callback')
     );
-});
+};
+
+Route::get('/', $redirectToAuth);
+Route::get('/login', $redirectToAuth);
 
 /*
 |--------------------------------------------------------------------------
