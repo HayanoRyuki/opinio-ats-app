b35edafe84f1ecbd1f4605b003d85f50a4fe98b4
HayanoRyuki <hayano@media-confidence.com>
2026-01-07 15:35:51 +0900

    add: placeholder candidates index view

---
+++ resources/views/candidates/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.ats')
+
+@section('content')
+    <h1 style="font-size:20px; font-weight:600; margin-bottom:16px;">
+        候補者一覧
+    </h1>
+
+    <p>
+        この画面は現在準備中です。
+    </p>
+@endsection
